b5e38f9d62da8980af5672dc1d03abcd10f708cb
Patrick Lavin <plavin997@gmail.com>
2021-12-04 16:56:39 -0500

    remove extra file

--- generate-sst-info-files.sh
+++
@@ -1,9 +0,0 @@
-elements=$(sst-info 2>/dev/null | grep ELEMENT | tail -n +2 | cut -d' ' -f 4)
-
-for ele in $elements
-do
-    comp="$comp"$(sst-info $ele | grep "SubComponent " | tr -d [:blank:] | sed 's/SubComponent.*://' | sed "s/^/$ele./")
-done
-
-echo $elements > elements.txt
-echo $comp > components.txt
